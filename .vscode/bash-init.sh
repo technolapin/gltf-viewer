0274860d5fc96a470fb92ef018dd70b8d55502d0
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2021-02-11 09:31:41 +0100

    Add utility code for tutorial

--- .vscode/bash-init.sh
+++ .vscode/bash-init.sh
@@ -38,3 +38,32 @@ cmake_install() {
   # e.g: cmake_install --config release to build in release mode, then install, with Visual Studio Compiler
   cmake --build build -j --target install $@
 }
+
+view_sponza() {
+  cmake_prepare
+  cmake_install
+  dist/gltf-viewer viewer gltf-sample-models/2.0/Sponza/glTF/Sponza.gltf \
+    --lookat -5.26056,6.59932,0.85661,-4.40144,6.23486,0.497347,0.342113,0.931131,-0.126476
+}
+
+view_helmet() {
+  cmake_prepare
+  cmake_install
+  dist/gltf-viewer viewer gltf-sample-models/2.0/DamagedHelmet/glTF/DamagedHelmet.gltf
+}
+
+render_sponza() {
+  cmake_prepare
+  cmake_install
+  [ ! -d output-images ] && mkdir output-images
+  dist/gltf-viewer viewer gltf-sample-models/2.0/Sponza/glTF/Sponza.gltf \
+    --lookat -5.26056,6.59932,0.85661,-4.40144,6.23486,0.497347,0.342113,0.931131,-0.126476 \
+    --output output-images/sponza.png
+}
+
+render_helmet() {
+  cmake_prepare
+  cmake_install
+  dist/gltf-viewer viewer gltf-sample-models/2.0/DamagedHelmet/glTF/DamagedHelmet.gltf \
+    --output output-images/helmet.png
+}
