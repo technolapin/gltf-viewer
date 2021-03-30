f07fbf17907e6a20d84428c656e66f52826238f9
technolapin <technolapin@orange.fr>
2021-03-30 03:08:03 +0200

    camera controller gui

--- src/ViewerApplication.cpp
+++ src/ViewerApplication.cpp
@@ -227,59 +227,66 @@ int ViewerApplication::run()
   // Loop until the user closes the window
   for (auto iterationCount = 0u; !m_GLFWHandle.shouldClose();
        ++iterationCount) {
-    const auto seconds = glfwGetTime();
+      const auto seconds = glfwGetTime();
 
-    const auto camera = cameras[camera_index]->getCamera();
-    drawScene(camera);
+      const auto camera = cameras[camera_index]->getCamera();
+      drawScene(camera);
 
-    // GUI code:
-    imguiNewFrame();
+      // GUI code:
+      imguiNewFrame();
 
-    {
-      ImGui::Begin("GUI");
-      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-          1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-      if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen)) {
-          ImGui::RadioButton("Trackball", &camera_index, 0); ImGui::SameLine();
-          ImGui::RadioButton("FPS", &camera_index, 1);
+      {
+          ImGui::Begin("GUI");
+          ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                      1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+          if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen)) {
 
-        ImGui::Text("eye: %.3f %.3f %.3f", camera.eye().x, camera.eye().y,
-            camera.eye().z);
-        ImGui::Text("center: %.3f %.3f %.3f", camera.center().x,
-            camera.center().y, camera.center().z);
-        ImGui::Text(
-            "up: %.3f %.3f %.3f", camera.up().x, camera.up().y, camera.up().z);
+              const auto prev_camera_index = camera_index;
+              auto cam_changed = ImGui::RadioButton("Trackball", &camera_index, 0);
+              ImGui::SameLine(); // I want to keep that
+              cam_changed = cam_changed || ImGui::RadioButton("FPS", &camera_index, 1);
+              if (cam_changed)
+              {
+                  cameras[camera_index]->setCamera(cameras[prev_camera_index]->getCamera());
+              }
 
-        ImGui::Text("front: %.3f %.3f %.3f", camera.front().x, camera.front().y,
-            camera.front().z);
-        ImGui::Text("left: %.3f %.3f %.3f", camera.left().x, camera.left().y,
-            camera.left().z);
+              ImGui::Text("eye: %.3f %.3f %.3f", camera.eye().x, camera.eye().y,
+                          camera.eye().z);
+              ImGui::Text("center: %.3f %.3f %.3f", camera.center().x,
+                          camera.center().y, camera.center().z);
+              ImGui::Text(
+                  "up: %.3f %.3f %.3f", camera.up().x, camera.up().y, camera.up().z);
 
-        if (ImGui::Button("CLI camera args to clipboard")) {
-          std::stringstream ss;
-          ss << "--lookat " << camera.eye().x << "," << camera.eye().y << ","
-             << camera.eye().z << "," << camera.center().x << ","
-             << camera.center().y << "," << camera.center().z << ","
-             << camera.up().x << "," << camera.up().y << "," << camera.up().z;
-          const auto str = ss.str();
-          glfwSetClipboardString(m_GLFWHandle.window(), str.c_str());
-        }
+              ImGui::Text("front: %.3f %.3f %.3f", camera.front().x, camera.front().y,
+                          camera.front().z);
+              ImGui::Text("left: %.3f %.3f %.3f", camera.left().x, camera.left().y,
+                          camera.left().z);
+
+              if (ImGui::Button("CLI camera args to clipboard")) {
+                  std::stringstream ss;
+                  ss << "--lookat " << camera.eye().x << "," << camera.eye().y << ","
+                     << camera.eye().z << "," << camera.center().x << ","
+                     << camera.center().y << "," << camera.center().z << ","
+                     << camera.up().x << "," << camera.up().y << "," << camera.up().z;
+                  const auto str = ss.str();
+                  glfwSetClipboardString(m_GLFWHandle.window(), str.c_str());
+              }
+          }
+          ImGui::End();
       }
-      ImGui::End();
-    }
 
-    imguiRenderFrame();
+      imguiRenderFrame();
 
-    glfwPollEvents(); // Poll for and process events
+      glfwPollEvents(); // Poll for and process events
 
-    auto ellapsedTime = glfwGetTime() - seconds;
-    auto guiHasFocus =
-        ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard;
-    if (!guiHasFocus) {
-      cameras[camera_index]->update(float(ellapsedTime));
-    }
+      auto ellapsedTime = glfwGetTime() - seconds;
+      auto guiHasFocus =
+          ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard;
+      if (!guiHasFocus) {
+          cameras[camera_index]->update(float(ellapsedTime));
+      }
 
-    m_GLFWHandle.swapBuffers(); // Swap front and back buffers
+      m_GLFWHandle.swapBuffers(); // Swap front and back buffers
   }
 
   // TODO clean up allocated GL data
@@ -290,9 +297,9 @@ int ViewerApplication::run()
 
 
 ViewerApplication::ViewerApplication(const fs::path &appPath, uint32_t width,
-    uint32_t height, const fs::path &gltfFile,
-    const std::vector<float> &lookatArgs, const std::string &vertexShader,
-    const std::string &fragmentShader, const fs::path &output) :
+                                     uint32_t height, const fs::path &gltfFile,
+                                     const std::vector<float> &lookatArgs, const std::string &vertexShader,
+                                     const std::string &fragmentShader, const fs::path &output) :
     m_nWindowWidth(width),
     m_nWindowHeight(height),
     m_AppPath{appPath},
@@ -302,29 +309,29 @@ ViewerApplication::ViewerApplication(const fs::path &appPath, uint32_t width,
     m_gltfFilePath{gltfFile},
     m_OutputPath{output}
 {
-  if (!lookatArgs.empty()) {
-    m_hasUserCamera = true;
-    m_userCamera =
-        Camera{glm::vec3(lookatArgs[0], lookatArgs[1], lookatArgs[2]),
+    if (!lookatArgs.empty()) {
+        m_hasUserCamera = true;
+        m_userCamera =
+            Camera{glm::vec3(lookatArgs[0], lookatArgs[1], lookatArgs[2]),
             glm::vec3(lookatArgs[3], lookatArgs[4], lookatArgs[5]),
             glm::vec3(lookatArgs[6], lookatArgs[7], lookatArgs[8])};
-  }
+    }
 
-  if (!vertexShader.empty()) {
-    m_vertexShader = vertexShader;
-  }
+    if (!vertexShader.empty()) {
+        m_vertexShader = vertexShader;
+    }
 
-  if (!fragmentShader.empty()) {
-    m_fragmentShader = fragmentShader;
-  }
+    if (!fragmentShader.empty()) {
+        m_fragmentShader = fragmentShader;
+    }
 
-  ImGui::GetIO().IniFilename =
-      m_ImGuiIniFilename.c_str(); // At exit, ImGUI will store its windows
-                                  // positions in this file
+    ImGui::GetIO().IniFilename =
+        m_ImGuiIniFilename.c_str(); // At exit, ImGUI will store its windows
+    // positions in this file
 
-  glfwSetKeyCallback(m_GLFWHandle.window(), keyCallback);
+    glfwSetKeyCallback(m_GLFWHandle.window(), keyCallback);
 
-  printGLVersion();
+    printGLVersion();
 }
 
 
